c3612694777ab7828a73ab53a796cc0c1194317b
Saydullayev017 <saydullayevjavlonbek2002@gmail.com>
2024-11-24 05:39:56 +0500

    Данный код реализует функциональность отправки сообщений в чат. Добавил две функциию 1. Функция для создания элемента сообщения, 2. Обработка исходящего сообщения.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-// объявляем константу и выбераем элемент с помошю querySelector 
+// Объявление константу и выбераем элемент с помошю querySelector 
+const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
 
+// Функция для создания элемента сообщения
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+// Обработка исходящего сообщения
+const hendleOutgoingMessage = (userMessage) => {
+    const messageContent = `<div class="message-text">${userMessage}</div>`;
+
+    const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+    chatBody.appendChild(outgoingMessageDiv);
+}
+
+// Обработчик события нажатия клавиши
 /*  Когда пользователь вводит текст и нажимает клавишу "Enter",
  сообщение выводится в консоль, если оно не пустое */
 messageInput.addEventListener("keydown", (e) =>{
@@ -8,6 +25,6 @@ messageInput.addEventListener("keydown", (e) =>{
     const userMessage = e.target.value.trim();
     // Проверка условия и вывод сообщения
     if(e.key === "Enter" && userMessage) {
-        console.log(userMessage)
+        hendleOutgoingMessage(userMessage);
     }
 })
