7c9ffe1f08e0f2a3a356483783778354ca245e0a
Saydullayev017 <saydullayevjavlonbek2002@gmail.com>
2024-11-24 06:08:16 +0500

    Расширел функциональность Обработку исходящего сообщения

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Объявление константу и выбераем элемент с помошю querySelector 
 const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
+const sendMessageButton = document.querySelector("#send-messsage");
 
+const userData ={
+    message: null
+}
 // Функция для создания элемента сообщения
 const createMessageElement = (content, classes) => {
     const div = document.createElement("div");
@@ -10,11 +14,16 @@ const createMessageElement = (content, classes) => {
     return div;
 }
 // Обработка исходящего сообщения
-const hendleOutgoingMessage = (userMessage) => {
-    const messageContent = `<div class="message-text">${userMessage}</div>`;
+const hendleOutgoingMessage = (e) => {
+    e.preventDefault(); // Предотвращаем стандартное поведение формы (если используется)
+    userData.message = messageInput.value.trim(); // Получаем текст из поля ввода
+    messageInput.value = ''; // После отправки сообщения стоит очищать поле ввода
 
+    const messageContent = `<div class="message-text"></div>`;
     const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
-    chatBody.appendChild(outgoingMessageDiv);
+    outgoingMessageDiv.querySelector(".message-text").textContent = userData.message; // Устанавливаем текст сообщения
+
+    chatBody.appendChild(outgoingMessageDiv); // Добавляем сообщение в область чата
 }
 
 // Обработчик события нажатия клавиши
@@ -22,9 +31,12 @@ const hendleOutgoingMessage = (userMessage) => {
  сообщение выводится в консоль, если оно не пустое */
 messageInput.addEventListener("keydown", (e) =>{
     // Здесь мы получаем текущее значение поля ввода (то есть текст, который пользователь ввел).
-    const userMessage = e.target.value.trim();
+    const userMessage = e.target.value.trim(); // // Проверяем, нажата ли клавиша "Enter" и не пустое ли сообщение
     // Проверка условия и вывод сообщения
     if(e.key === "Enter" && userMessage) {
-        hendleOutgoingMessage(userMessage);
+        hendleOutgoingMessage(e);
     }
 })
+
+// Обработчик события нажатия кнопки отправки
+sendMessageButton.addEventListener("click", (e) => hendleOutgoingMessage(e));
